8f934f31d700b25bb83df958948739e1eae39ee9
whats2000 <whats2000mc@gmail.com>
2023-11-19 18:56:08 +0800

    Update SelectorSetting.vue

---
+++ app/src/components/SelectorSetting.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="selector-setting">
+   Nothing Currently
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectorSetting',
+};
+</script>
+
+<style>
+</style>
